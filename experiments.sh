b9bda1a13eea001310b21de131443c680af16e0a
ilya <iliyadimov@icloud.com>
2021-02-03 23:37:17 +0300

    Add gpu flag

--- experiments.sh
+++ experiments.sh
@@ -1,11 +1,11 @@
 python train.py --serialization_dir custom_transformer --d_model 512 --num_layers 6 --num_heads 8 --ff_dim 2048 \
 --dropout 0.2 --model_type transformer --max_length 2010 --warmup_steps 1000 --batch_size 32 --base_lr 0.00002 \
---weight_decay 0.99
+--weight_decay 0.99 --gpus 1
 
 
 python train.py --serialization_dir pytorch_transformer --d_model 512 --num_layers 6 --num_heads 8 --ff_dim 2048 \
 --dropout 0.2 --model_type pytorch_transformer --max_length 2010 --warmup_steps 1000 --batch_size 32 --base_lr 0.00002 \
---weight_decay 0.99
+--weight_decay 0.99 --gpus 1
 
 python train.py --serialization_dir lstm --d_model 512 --num_layers 2 \
---dropout 0.2 --model_type lstm --warmup_steps 100 --batch_size 32 --base_lr 0.001
+--dropout 0.2 --model_type lstm --warmup_steps 100 --batch_size 32 --base_lr 0.001 --gpus 1
